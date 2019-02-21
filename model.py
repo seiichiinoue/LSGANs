9f8fc178e7520a32096ed66fc99988a39f69aeb8
Seiichi Inoue <d35inou108@gmail.com>
2019-02-21 22:20:42 +0900

    updated model

--- model.py
+++ model.py
@@ -13,8 +13,6 @@ import matplotlib.pyplot as plt
 class Generator(nn.Module):
     """
     This model is copies of Least Squares Generative Adversarial Networks.
-    On the paper, all the output in Generator is batch normalized.
-    so, added batch-norm layer after every layer except from last output.
     """
 
     def __init__(self):
@@ -25,16 +23,17 @@ class Generator(nn.Module):
         self.fc1 = nn.Linear(in_features=256, out_features=1024)
         self.fc2 = nn.Linear(in_features=1024, out_features=7 * 7 * 128)
         self.fc2_bn = nn.BatchNorm2d(7 * 7 * 128)
+        
         # deconvolutional layer
         self.deconv1 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=5, stride=2)
         self.deconv1_bn = nn.BatchNorm2d(128)
+        
         # output
         self.deconv2 = nn.ConvTranspose2d(in_channels=128, out_channels=1, kernel_size=5, stride=2)
 
 
-
     def forward(self, x):
-        
+
         x = self.fc1(x)
         x = self.fc2_bn(self.fc2(x))
         x = x.view(-1, 128, 7, 7)
@@ -59,6 +58,7 @@ class Discriminator(nn.Module):
         self.fc1_bn = nn.BatchNorm1d(1024)
         self.fc2 = nn.Linear(in_features=1024, out_features=1)
 
+
     def forward(self, x):
 
         x = self.conv1(x)
@@ -68,3 +68,7 @@ class Discriminator(nn.Module):
 
         return self.fc2(x)
 
+
+if __name__ == '__main__':
+    print(Generator())
+    print(Discriminator())
