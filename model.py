8dc2407a3ef1c79f19b39d67403df804a4e13d49
Seiichi Inoue <d35inou108@gmail.com>
2019-02-22 02:48:59 +0900

    added lsgan model

--- model.py
+++ model.py
@@ -8,61 +8,64 @@ from torchvision.utils import save_image
 
 
 class Generator(nn.Module):
-
+    
     def __init__(self):
         super(Generator, self).__init__()
-        # fully connected layer
+        # fully connected
         self.fc = nn.Sequential(
-            nn.Linear(256, 1024),
+            nn.Linear(62, 1024),
+            nn.BatchNorm1d(1024),
             nn.ReLU(),
-            nn.Linear(1024, 7 * 7 * 128),
-            nn.BatchNorm1d(7 * 7 * 128),
-            nn.ReLU()
+            nn.Linear(1024, 128 * 7 * 7),
+            nn.BatchNorm1d(128 * 7 * 7),
+            nn.ReLU(),
         )
         # deconvolutional layer
         self.deconv = nn.Sequential(
-            nn.ConvTranspose2d(128, 128, kernel_size=5, stride=2),
-            nn.BatchNorm2d(128),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 1, kernel_size=5, stride=2),
-            nn.Sigmoid()
+            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1),
+            nn.Sigmoid(),
         )
 
-    def forward(self, x):
-        x = self.fc(x)
+    def forward(self, input):
+        x = self.fc(input)
         x = x.view(-1, 128, 7, 7)
         x = self.deconv(x)
         return x
 
 
 class Discriminator(nn.Module):
+    
     def __init__(self):
         super(Discriminator, self).__init__()
         # convolutional layer
         self.conv = nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=5, stride=2),
-            nn.LeakyReLU(),
-            nn.Conv2d(256, 320, kernel_size=5, stride=2),
-            nn.BatchNorm2d(320),
-            nn.LeakyReLU()
-        )    
-        # fully connected layer
+            nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+        )
+        # fully connected
         self.fc = nn.Sequential(
             nn.Linear(128 * 7 * 7, 1024),
             nn.BatchNorm1d(1024),
-            nn.LeakyReLU(),
+            nn.LeakyReLU(0.2),
             nn.Linear(1024, 1),
-            nn.Sigmoid()
+            nn.Sigmoid(),
         )
-
-    def forward(self, x):
-        x = self.conv(x)
+    
+    def forward(self, input):
+        x = self.conv(input)
         x = x.view(-1, 128 * 7 * 7)
         x = self.fc(x)
         return x
 
-
 if __name__ == '__main__':
+    
     # check model shape
     print(Generator())
     print(Discriminator())
+
