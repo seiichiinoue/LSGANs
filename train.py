cab92b3ecaae85655c6a0f9f290fd90b0078484d
Seiichi Inoue <d35inou108@gmail.com>
2019-02-22 02:49:13 +0900

    added trainer

--- train.py
+++ train.py
@@ -4,34 +4,24 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
 import matplotlib.pyplot as plt
-
+# my modules
 from model import *
 
-def train_itr(dataset, batch_size=128):
+
+def train(D, G, train_itr, epoch, batch_size=128, z_dim=62):
     
-    idx = np.random.permutation(len(dataset.train_labels))
-    X, y = [], []
-    for j, i in enumerate(idx):
-        X.append(dataset.train_data[i])
-        y.append(dataset.train_labels[i])
-        if j + 1 % batch_size == 0:
-            yield (torch.from_numpy(np.array(X)).float(), torch.from_numpy(np.array(y)).float())
-            X, y = [], []
-
-
-def train(dataset, epoch, batch_size=128, z_dim=28 * 28):
-
-    D = Discriminator()
-    G = Generator()
-
-    criterion = nn.MSELoss()
+    # settings
     D_optimizer = optim.Adam(D.parameters())
     G_optimizer = optim.Adam(G.parameters())
+    
+    # labels
+    a = 0
+    b = 1
+    c = 1
 
     for i in range(epoch):
 
@@ -42,7 +32,7 @@ def train(dataset, epoch, batch_size=128, z_dim=28 * 28):
         D_running_loss = 0
         G_running_loss = 0
 
-        for batch_index, (real_img, _) in enumerate(train_itr(dataset=dataset, batch_size=batch_size)):
+        for batch_index, (real_img, _) in enumerate(train_itr):
 
             if real_img.size()[0] != batch_size:
                 break
@@ -55,15 +45,15 @@ def train(dataset, epoch, batch_size=128, z_dim=28 * 28):
 
             # real
             D_real = D(real_img)
-            D_real_loss = criterion(D_real, y_real)
+            D_real_loss = torch.sum((D_real - b) ** 2)
 
             # fake
-            faek_img = G(z)
-            D_fake = D(faek_img.detach())  # stop propagate to G
-            D_fake_loss = criterion(D_fake, y_fake)
+            fake_img = G(z)
+            D_fake = D(fake_img.detach())  # stop propagate to G
+            D_fake_loss = torch.sum((D_fake - a) ** 2)
 
             # minimizing loss
-            D_loss = D_real_loss + D_fake_loss
+            D_loss = 0.5 * (D_real_loss + D_fake_loss) / batch_size
             D_loss.backward()
             D_optimizer.step()
             D_running_loss += D_loss.data.item()
@@ -74,23 +64,34 @@ def train(dataset, epoch, batch_size=128, z_dim=28 * 28):
             fake_img = G(z)
             D_fake = D(fake_img)
             
-            G_loss = criterion(D_fake, y_real)
+            G_loss = 0.5 * (torch.sum((D_fake - c) ** 2)) / batch_size
             G_loss.backward()
             G_optimizer.step()
             G_running_loss += G_loss.data.item()
 
-            print('epoch: {}, discriminator loss: {}, generator loss: {}'.format(i, D_running_loss, G_running_loss))
+        print('epoch: {}, discriminator loss: {}, generator loss: {}'.format(i, D_running_loss, G_running_loss))
 
     torch.save(G.state_dict(), 'model/generator.pth')
     torch.save(D.state_dict(), 'model/discriminator.pth')
 
+
 if __name__ == '__main__':
     
-    dataset = datasets.MNIST('data/mnist', train=True, download=True)  
+    # args
     epoch = 30
-    z_dim = dataset.train_data[0].shape[0] ** 2
+    z_dim = 62
     batch_size = 128
 
-    train(dataset, epoch, batch_size, z_dim)
+    # dataset
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    dataset = datasets.MNIST('data/mnist', train=True, download=True, transform=transform)
+    train_itr = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
+    # build modules
+    D = Discriminator()
+    G = Generator()
+
+    train(D, G, train_itr, epoch, batch_size, z_dim)
 
