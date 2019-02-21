43beea1f5298922013d119e33e3fca7cdd32b076
Seiichi Inoue <d35inou108@gmail.com>
2019-02-21 19:57:46 +0900

    added model

--- model.py
+++ model.py
@@ -11,24 +11,60 @@ import matplotlib.pyplot as plt
 
 
 class Generator(nn.Module):
-    """This model is copies of Least Squares Generative Adversarial Networks."""
+    """
+    This model is copies of Least Squares Generative Adversarial Networks.
+    On the paper, all the output in Generator is batch normalized.
+    so, added batch-norm layer after every layer except from last output.
+    """
 
     def __init__(self):
         
         super(Generator, self).__init__()
 
         # fully connected layer
-        self.fc = nn.Linear(in_features=1024, out_features=256)
-        
+        self.fc1 = nn.Linear(in_features=256, out_features=1024)
+        self.fc2 = nn.Linear(in_features=1024, out_features=7 * 7 * 128)
+        self.fc2_bn = nn.BatchNorm2d(7 * 7 * 128)
         # deconvolutional layer
-        self.deconv1 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=3, stride=2)
-        self.deconv2 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=3, stride=1)
-        self.deconv3 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=3, stride=2)
-        self.deconv4 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=3, stride=1)
-        self.deconv5 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=2)
-        self.deconv6 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2)
-        self.deconv7 = nn.ConvTranspose2d(in_channels=64, out_channels=3, kernel_size=3, stride=1)
+        self.deconv1 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=5, stride=2)
+        self.deconv1_bn = nn.BatchNorm2d(128)
+        # output
+        self.deconv2 = nn.ConvTranspose2d(in_channels=128, out_channels=1, kernel_size=5, stride=2)
+
 
 
     def forward(self, x):
+        
+        x = self.fc1(x)
+        x = self.fc2_bn(self.fc2(x))
+        x = x.view(-1, 128, 7, 7)
+        x = self.deconv1_bn(self.deconv1(x))
+
+        return self.deconv2(x)
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self):
+
+        super(Discriminator, self).__init__()
+
+        # deconvolutional layer 
+        self.conv1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=5, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=256, out_channels=320, kernel_size=5, stride=2)
+        self.conv2_bn = nn.BatchNorm2d(320)
+
+        # fully connected layer
+        self.fc1 = nn.Linear(in_features=128 * 7 * 7, out_features=1024)
+        self.fc1_bn = nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(in_features=1024, out_features=1)
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.conv2_bn(self.deconv2(x))
+        x = x.view(-1, 128 * 7 * 7)
+        x = self.fc1_bn(self.fc1(x))
+
+        return self.fc2(x)
 
